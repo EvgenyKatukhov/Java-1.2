508e9657d5239983c27130e801d847e030e68b04
EvgenyKatukhov <96973714+EvgenyKatukhov@users.noreply.github.com>
2022-03-06 11:19:16 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MilesService service = new MilesService();
+        Bonus service = new Bonus();
         int result = service.calculate(1000, 100);
         System.out.println("На вашем счете с учетом бонуса " + result);
     }
