8a6bbddc71163b50c89f1eee52fc2fc429d94e55
EvgenyKatukhov <96973714+EvgenyKatukhov@users.noreply.github.com>
2022-03-14 21:59:18 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,21 @@ public class Main {
         int result = service.calculate(1000, 100);
         System.out.println("На вашем счете с учетом бонуса " + result);
     }
-}
+     public int calculate(int increase, int fix) {
+        if (increase >= 1000) {
+            int bonus = increase / 100;
+            int ent = fix + bonus + increase;
+            System.out.println("На вашем счету " + fix);
+            System.out.println("Вы пополнили счет на " + increase);
+            return ent;
+        } else {
+            int ent = fix + increase;
+            System.out.println("Ваш счет равен " + ent);
+            return ent;
+        }
+
+    }
+
 
 
 
